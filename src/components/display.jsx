edcd586a936224fefd0e6f58f2f578e2c19d6159
Tatjana Devrnja <devrnjat94@gmail.com>
2020-09-02 17:48:21 +0200

    mapStateToProps function and modify Display

--- src/components/display.jsx
+++ src/components/display.jsx
@@ -1,15 +1,36 @@
 import React from "react";
+import { connect } from "react-redux";
+import "../custom.css";
 
 const Display = (props) => {
-  if (!props.value) {
+  if (!props.name) {
     return null;
   } else {
     return (
       <div>
-        <div className="output">{props.value}</div>
+        <div className="box">
+          <div className="name">{props.name}</div>
+        </div>
+        <div className="box1">
+          <div className="age">Age: {props.age}</div>
+        </div>
+        <div className="box2">
+          <div className="countries">Country: {props.countries}</div>
+        </div>
+        <div className="box3">
+          <div className="gender">Gender: {props.gender}</div>
+        </div>
       </div>
     );
   }
 };
 
-export default Display;
+const mapStateToProps = (state) => {
+  return {
+    name: state.name,
+    countries: state.countries,
+    age: state.age,
+    gender: state.gender,
+  };
+};
+export default connect(mapStateToProps)(Display);
