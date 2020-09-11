404b9b7451f53805f1ae05eddab70d372f228bd6
Tatjana Devrnja <devrnjat94@gmail.com>
2020-09-11 19:38:56 +0200

    updated mainReducer

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,18 +1,23 @@
+import { UPDATE_INFO } from "../action-types/index";
 let defaultState = {
   name: "",
   age: "",
   countries: "",
   gender: "",
+  countryIds: [],
+  countryPopularity: [],
 };
 
 const mainReducer = (state = defaultState, action) => {
   switch (action.type) {
-    case "NEW_DATA": {
+    case UPDATE_INFO: {
       return {
         name: action.name,
         age: action.age,
         countries: action.countries,
         gender: action.gender,
+        countryIds: action.countryIds,
+        countryPopularity: action.countryPopularity,
       };
     }
     default:
