b8ae93ac2a8bbd43473c59868c8cc4cf68b92185
Tatjana Devrnja <devrnjat94@gmail.com>
2020-09-20 18:16:25 +0200

    globe and map coords

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,24 +1,41 @@
 import { UPDATE_INFO } from "../action-types/index";
+import { UPDATE_GLOBE_COORDS } from "../action-types/index";
+import { UPDATE_MAP_COORDS } from "../action-types/index";
 
 let defaultState = {
   name: "",
   age: "",
-  countries: "",
+  countries: [],
   gender: "",
   countryIds: [],
-  countryPopularity: [],
+  namePopularity: [],
+  globeCoords: [0, 0],
+  mapCoords: [0, 0],
 };
 
 const mainReducer = (state = defaultState, action) => {
   switch (action.type) {
     case UPDATE_INFO: {
       return {
+        ...state,
         name: action.name,
         age: action.age,
         countries: action.countries,
         gender: action.gender,
         countryIds: action.countryIds,
-        countryPopularity: action.countryPopularity,
+        namePopularity: action.namePopularity,
+      };
+    }
+    case UPDATE_GLOBE_COORDS: {
+      return {
+        ...state,
+        globeCoords: action.globeCoords,
+      };
+    }
+    case UPDATE_MAP_COORDS: {
+      return {
+        ...state,
+        mapCoords: action.mapCoords,
       };
     }
     default:
