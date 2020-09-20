a0c834502f8e46715cce86a35bc4e52fba325065
Tatjana Devrnja <devrnjat94@gmail.com>
2020-09-20 17:44:58 +0200

    added globe and map coords

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,5 +1,7 @@
 import requestApi from "../api/request";
 import { UPDATE_INFO } from "../action-types/index";
+import { UPDATE_GLOBE_COORDS } from "../action-types/index";
+import { UPDATE_MAP_COORDS } from "../action-types/index";
 
 export function loadNameInfo() {
   return (dispatch) => {
@@ -17,5 +19,19 @@ const updateInfo = (data) => {
   return {
     type: UPDATE_INFO,
     ...data,
+  };
+};
+
+export const updateGlobeCoords = (coords) => {
+  return {
+    type: UPDATE_GLOBE_COORDS,
+    globeCoords: coords,
+  };
+};
+
+export const updateMapCoords = (coords) => {
+  return {
+    type: UPDATE_MAP_COORDS,
+    mapCoords: coords,
   };
 };
