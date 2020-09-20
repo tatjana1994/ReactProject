dff18025b57eb5cad7bf6e0ef0ed89e0765458f3
Tatjana Devrnja <devrnjat94@gmail.com>
2020-09-20 18:13:49 +0200

    background, DisplayGlobe..

--- src/index.js
+++ src/index.js
@@ -1,21 +1,34 @@
 import ReactDOM from "react-dom";
-import Input from "./components/input";
-import Display from "./components/display";
+import InputField from "./components/InputField";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import reducers from "./reducers/reducers";
 import React from "react";
-import DisplayMap from "./components/displayMap";
+import DisplayMap from "./components/DisplayMap";
+import DisplayContent from "./components/DisplayContent";
+import "./tailwind.output.css";
+import DisplayGlobe from "./components/DisplayGlobe";
+import Background from "./images/background.jpg";
 
 let store = createStore(reducers, applyMiddleware(thunk));
 
 const App = () => {
   return (
-    <div>
-      <DisplayMap />
-      <Input />
-      <Display />
+    <div
+      style={{ backgroundImage: "url(" + Background + ")" }}
+      className=" w-screen h-screen fixed"
+    >
+      <InputField />
+      <div className="flex flex-row">
+        <div className="w-2/4 ml-0 -mt-0 pb-4 flex flex-wrap justify-center">
+          <DisplayMap />
+        </div>
+        <div className="ml-20">
+          <DisplayGlobe />
+        </div>
+      </div>
+      <DisplayContent />
     </div>
   );
 };
